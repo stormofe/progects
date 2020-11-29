21e6a448a71879d3d40396d883e453414b0f870c
stormofe <stormofe@gmail.com>
2020-11-29 18:50:02 +0200

    optim

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
+                    dots: false,
                     arrows: false
                 }
             }
